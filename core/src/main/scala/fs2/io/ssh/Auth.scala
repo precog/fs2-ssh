f60c17b089d9f52a9857e638ee31dd9adef59914
Daniel Spiewak <daniel@slamdata.com>
2019-08-22 17:00:10 -0600

    Fleshed out skeleton of what I'm thinking

---
+++ core/src/main/scala/fs2/io/ssh/Auth.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2014–2019 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fs2
+package io.ssh
+
+import scala.{Array, Option, None, Product, Serializable}
+
+import java.lang.{String, SuppressWarnings}
+import java.nio.file.Path
+
+@SuppressWarnings(Array("org.wartremover.warts.DefaultArguments"))
+sealed trait Auth extends Product with Serializable
+
+object Auth {
+  final case class Password(text: String) extends Auth
+  final case class Key(privateKey: Path, password: Option[String] = None) extends Auth
+}
